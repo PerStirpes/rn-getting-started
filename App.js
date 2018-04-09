3225b22f8c4e806bd570c2b294cc62fbdfeba78b
Matt Barraza-Carl <PerStirpes@users.noreply.github.com>
2018-04-08 23:14:18 -0700

    add basics, props

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { AppRegistry, Image, StyleSheet, Text, View } from "react-native";
 
-export default class App extends React.Component {
+class Greeting extends Component {
   render() {
+    return <Text> Hello {this.props.name}!</Text>;
+  }
+}
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    let pic = {
+      uri:
+        "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg",
+    };
+
     return (
       <View style={styles.container}>
+        <Image source={pic} style={{ width: 193, height: 110 }} />
+        <Greeting name="Rex" />
+        <Greeting name="Bill" />
+        <Greeting name="Lorena" />
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
@@ -16,8 +31,10 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+AppRegistry.registerComponent("AwesomeProject", () => LotsOfGreetings);
